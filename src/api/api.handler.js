156d11f1dca0a52d0049942fb304748891d56fe3
fedgut <fedgut@gmail.com>
2020-04-14 16:33:39 -0500

    add content security policy

--- src/api/api.handler.js
+++ src/api/api.handler.js
@@ -2,8 +2,7 @@
 async function data(...params) {
   const key = 'ce60013986dfcb3ebf13accf07bbb9f9';
   const response = await fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=${params[0]}&units=${params[1]}&appid=${key}`,
-    { mode: 'cors' },
+    `http://api.openweathermap.org/data/2.5/weather?q=${params[0]}&units=${params[1]}&appid=${key}`, { mode: 'cors' },
   );
   const getData = await response.json();
   params[1] === 'metric' ? getData.units = '°C' : getData.units = '°F';
