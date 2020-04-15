6338c416215e82ec574c5d372cfcfe8a366f206b
fedgut <fedgut@gmail.com>
2020-04-15 11:46:08 -0500

    add babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,16 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
     ],
   },
 };
