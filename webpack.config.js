f885a63b59806d603b59d109e4555b94a97d4c86
fedgut <fedgut@gmail.com>
2020-04-15 14:58:09 -0500

    remove loader causing issues

--- webpack.config.js
+++ webpack.config.js
@@ -19,16 +19,16 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
+      // {
+      //   test: /\.m?js$/,
+      //   exclude: /(node_modules|bower_components)/,
+      //   use: {
+      //     loader: 'babel-loader',
+      //     options: {
+      //       presets: ['@babel/preset-env'],
+      //     },
+      //   },
+      // },
     ],
   },
 };
